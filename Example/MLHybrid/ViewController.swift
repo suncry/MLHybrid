fcfabdf448d9ff01040b1e791d5047700eb3c027
yang cai <caiyang@medlinker.com>
2017-08-08 20:56:53 +0800

    ***	By Cy	**

--- Example/MLHybrid/ViewController.swift
+++ Example/MLHybrid/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MLHybrid
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //MLHybrid.say()
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
