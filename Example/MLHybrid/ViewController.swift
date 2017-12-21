4c82e377944396b9247cdfb908ffbf597186506e
liushuoyu <liushuoyu@medlinker.com>
2017-12-21 22:17:53 +0800

    AVPaly

--- Example/MLHybrid/ViewController.swift
+++ Example/MLHybrid/ViewController.swift
@@ -8,23 +8,48 @@
 
 import UIKit
 import MLHybrid
+import KDEAudioPlayer
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var  avplay:AVPlayer!
+    var  playerItem :AVPlayerItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let url  = URL(string:"http://7xpdel.com1.z0.glb.clouddn.com/luwEJUGXZSN76HyXUF4KBwIiGmNt")
+        playerItem = AVPlayerItem(url:url!)
+        avplay = AVPlayer(playerItem:playerItem)
+       
     }
 
     @IBAction func click(_ sender: Any) {
+        avplay.play()
 //        let dddd = MLHybrid.load(urlString: "http://web.qa.medlinker.com/h5/hospital/z_index.html?serviceType=1&type=d")
-        let dddd = MLHybrid.load(urlString: "http://web.qa.medlinker.com/h5/hospital/z_home.html")
-
-        let navi = UINavigationController(rootViewController: dddd!)
-        navi.navigationBar.isTranslucent = false
-        self.present(navi, animated: true, completion: nil)
+//        let dddd = MLHybrid.load(urlString: "http://7xpdel.com1.z0.glb.clouddn.com/luwEJUGXZSN76HyXUF4KBwIiGmNt")
+//
+//        let navi = UINavigationController(rootViewController: dddd!)
+//        navi.navigationBar.isTranslucent = false
+//        self.present(navi, animated: true, completion: nil)
         
     }
+    
+     override  func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        UIApplication.shared.beginIgnoringInteractionEvents()
+//        self.becomeFirstResponder()
+    
+//        let item = AudioItem(mediumQualitySoundURL: url!)
+//        player.play(item: item!)
+    }
 
+    
+//    override func remoteControlReceived(with event: UIEvent?) {
+//        if let event = event{
+//            player.remoteControlReceived(with: event)
+//
+//        }
+//    }
 }
 
